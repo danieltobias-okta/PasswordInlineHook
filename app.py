172ace7b3b17ab69acfa657beebcc252e1e2f379
danieltobias-okta <68609308+danieltobias-okta@users.noreply.github.com>
2020-08-26 12:57:33 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/password", methods = ['POST'])
+def password():
+    print(request.json)
+    response = request.json
+    
+    return jsonify(commands = [{"type":"com.okta.action.update", "value" : { "credential":"VERIFIED"}}])
+
+if __name__ == "__main__":
+    app.run(debug=True)
